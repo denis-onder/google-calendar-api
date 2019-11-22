e7667f0ee2390942b456f664a2d4273aec56f787
Denis Onder <denis_onder@protonmail.com>
2019-11-23 00:25:10 +0100

    Working on the dashboard styles

--- app/router.js
+++ app/router.js
@@ -1,5 +1,7 @@
 const { clientID, apiKey } = require("./config").oauth;
 
 module.exports = app => {
-  app.get("/", (_, res) => res.render("main", { clientID, apiKey }));
+  app.get("/", (_, res) =>
+    res.render("main", { file: "main", clientID, apiKey })
+  );
 };
